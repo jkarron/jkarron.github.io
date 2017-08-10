1031711f5629e0a5c86046cfe40a5ffab0c53372
Julia Karron <jkarron@umd.edu>
2017-08-10 15:15:16 -0400

    Insert carousel outline. Find photos that work and make sure to check data from CDC

--- HealthCareFinal/js/script.js
+++ HealthCareFinal/js/script.js
@@ -15,4 +15,16 @@ $(document).ready(function() {
       series.show();
     }
   });
+  // Activate Carousel
+$("#myCarousel").carousel();
+
+// Enable Carousel Indicators
+$(".item").click(function(){
+    $("#myCarousel").carousel(1);
+});
+
+// Enable Carousel Controls
+$(".left").click(function(){
+    $("#myCarousel").carousel("prev");
+});
 });
