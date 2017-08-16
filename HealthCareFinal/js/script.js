8d900d6b18788f5076d58b6c2f716cac69baa388
Julia Karron <jkarron@umd.edu>
2017-08-16 19:11:03 -0400

    Add preliminary javascript edits for navbar to function. Start edits on resume, about me, and portfolio incorporating chris's comments

--- HealthCareFinal/js/script.js
+++ HealthCareFinal/js/script.js
@@ -17,3 +17,13 @@ $(".left").click(function(){
     $("#myCarousel").carousel("prev");
 });
 });
+
+//Navbar controls. Add commands below when you know how they work
+$('.navbar-brand').click(function(){
+  $('.navbar-brand #1');
+  $('.navbar-brand #2');
+  $('.navbar-brand #3');
+  $('.navbar-brand #4');
+  $('.navbar-brand #5');
+  $('navbar-brand #6');  
+});
